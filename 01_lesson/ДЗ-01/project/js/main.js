015ec72f067a116ecf5980e337798fe7fa34b306
AndryAsh <ashikhminag@yandex.ru>
2021-12-28 22:40:38 +1000

    ДЗ-01 to correct

--- 01_lesson/ДЗ-01/project/js/main.js
+++ 01_lesson/ДЗ-01/project/js/main.js
@@ -1,14 +1,15 @@
 const products = [
   { id: 1, title: 'Notebook', price: 1000, img: './img/notebook-1200x630.webp' },
   { id: 2, title: 'Mouse', price: 100, img: './img/mouse-1200x630.webp' },
-  { id: 3, title: 'Keyboard', price: 250, img: '' },
-  { id: 4, title: 'Gamepad', price: 150, img: '' },
+  { id: 3, title: 'Keyboard', price: 250, img: null },
+  { id: 4, title: 'Gamepad', price: 150, img: null },
 ];
 
 const renderProduct = (title, price, img = './img/no_image-1200x630.png') => {
   /* if (img === '') {
     img = './img/no_image-1200x630.png';
   } */
+  console.log(img);
   return `<article class="product-item border-radius">
             <div class="product-img-wrap border-radius">
               <img src=${img} alt="Фотография товвара" class="product-img border-radius">
